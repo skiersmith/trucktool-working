151468768ca7a5b98add6268a480b6e50c00a746
Hunter Smith <skiersmith21@gmail.com>
2018-01-05 11:29:34 -0700

    pushh all.vue

--- www/src/components/All.vue
+++ www/src/components/All.vue
@@ -1,211 +1,137 @@
 <template>
-    <div class="productt col-sm-3">
-        <div class="l">
-                <h2 class="title" @click="prodSeen = !prodSeen">{{product.name}}</h2>
-        </div>
-        <div class="div" v-if="prodSeen">
-            <div class="product-container">
-                <div class="product-body">
-                    <p>Quantity: {{product.quantity}}</p>
-                    <p>Price: {{product.unitPrice}}</p>
-                    <p>Sell Price:{{product.resalePrice}}</p>
-                    <p>{{product.sku}}</p>
-                    <p>Tags: {{product.tag}}</p>
-
-                    <div class="the-lists">
-                        <form @change="moveProductToDifferentList">
-                            <p class="inline">List:</p>
-                            <select name="select list" v-model="formOption" class="listSelect">
-                                <option disabled selected>Select List</option>
-                                <option v-for="list in lists" :value="list._id">{{list.name}}</option>
-                            </select>
-                        </form>
-                    </div>
+        <div>
+            <div class="list-header">
+                <h3 class="list-text-color">{{list.name}}</h3>
+                <div v-if="description">
+                    <p class="list-text-color">Description: {{description}}</p>
                 </div>
-                <div class="open-notes">
-                    <p class="productTitle" @click="notesSeen = !notesSeen">
-                        <b>Notes: </b>
-                        <p class="add-note" v-if="notesSeen" @click="toggleNoteForm">Add Note
-                            <span class="fa fa-plus"></span>
-                        </p>
-                    </p>
-                    <div class="noteForm" v-if="showNoteForm">
-                        <div class="newNote">
-                            <form @submit.prevent="newNote">
-                                <div class="form-group">
-                                    <input placeholder="note" name="description" type="text" size="10" v-model="note.description">
-                                    <button class="btn-success btn-xs" type="submit">Add</button>
-                                </div>
-                            </form>
+            </div>
+            <div class="list-body">
+                <button class="btn-success btn-xs margin" @click="toggleProductForm">New Product</button>
+                <div class="create-product" v-if="showAddProductForm">
+                    <form @submit.prevent="createProduct">
+                        <div class="form-group">
+                            <input class="inline" size="15" type="text" name="name" placeholder="name" v-model="product.name" required>
+                            <input class="inline" size="15" type="text" name="quantity" placeholder="quantity" v-model="product.quantity" required>
+                            <input class="inline" size="15" type="text" name="unitPrice" placeholder="unitPrice" v-model="product.unitPrice" required>
+                            <input class="inline" size="15" type="text" name="resalePrice" placeholder="resalePrice" v-model="product.resalePrice">
+                            <input class="inline" size="15" type="text" name="sku" placeholder="sku" v-model="product.sku">
+                            <input class="inline" size="15" type="text" name="tag" placeholder="tag" v-model="product.tag">
+                            <button type="submit" class="btn-xs btn-success">Add</button>
                         </div>
-                    </div>
-                    <div class="notes" v-if="notesSeen" v-for="note in notes">
-                        <p>{{note.description}}</p>
-                        <timeago :since="note.created"></timeago>
-                        <i class="fa fa-minus" @click="removeNote(note._id)"></i>
-                    </div>
+                    </form>
                 </div>
-                <div class="removeProd">
-                    <P class="inline">Remove Product
-                        <i class="fa fa-trash delProd" @click="removeProduct"></i>
-                    </P>
+            </div>
+            <div class="list-footer row">
+                <div class="col-sm-offset-2" v-for="product in products">
+                    <product class="the-product" :product="product"></product>
                 </div>
             </div>
         </div>
-    </div>
-</template>
-
-<script>
-    import list from './List'
-    export default {
-        data() {
-            return {
-                showNoteForm: false,
-                note: {
-                    categoryId: this.categoryId,
-                    listId: this.listId,
-                    productId: this.product._id
-                },
-                formOption: '',
-                notesSeen: false,
-                prodSeen: false
-            }
-        },
-        name: 'all',
-        props: ['product', 'listId', 'newListId'],
-        mounted() {
-            this.$store.dispatch('getNotes', { productId: this.product._id, listId: this.listId })
-        },
-        methods: {
-            openNotes() {
-                this.$store.dispatch('getNotes', { productId: this.productId, listId: this.listId })
-            },
-            moveProductToDifferentList(newListId) {  
-                this.$store.dispatch('moveProductToDifferentList', { productId: this.product._id, oldListId: this.product.listId, listId: this.formOption })
-            },
-            newNote() {
-
-                this.$store.dispatch('newNote', { note: this.note })
-                this.note = {
-                    listId: this.product.listId,
-                    productId: this.product._id
+    </template>
+    <script>
+        import product from './Product'
+        export default {
+            data() {
+                return {
+                    list: {
+                        name: this.name,
+    
+                    },
+    
+                    product: {
+                        listId: this.listId,
+                        categoryId: this.categoryId,
+                    },
+                    showAddProductForm: false,
+                    newListId: document.getElementById('listId')
                 }
-                this.toggleNoteForm()
             },
-            removeProduct() {
-                this.$store.dispatch('removeProduct', { productId: this.product._id, listId: this.product.listId})
+            name: 'list',
+            props: ['name', 'description', 'listId', 'categoryId', 'productId', 'id'],
+            mounted() {
+    
+               
+                this.$store.dispatch('getProductsByList', { listId: this.product.listId, categoryId: this.categoryId })
             },
-            removeNote(noteId) {
-                this.$store.dispatch('removeNote', { productId: this.product._id, listId: this.listId, noteId: noteId })
+            methods: {
+    
+                createProduct() {
+                    // this.product.order = this.$store.state.activeProducts[this.listId].length
+                    this.$store.dispatch('createProduct', { product: this.product })
+                    this.product = {
+                        listId: this.product.listId,
+                        categoryId: this.categoryId
+                    }
+                    this.toggleProductForm()
+                },
+                toggleProductForm() {
+                    this.showAddProductForm = !this.showAddProductForm
+                }
             },
-            toggleNoteForm() {
-                this.showNoteForm = !this.showNoteForm
+            computed: {
+                products() {
+    
+                    return this.$store.state.activeProducts[this.listId]
+                },
+                lists() {
+                    return this.$store.state.activeLists
+                }
+            },
+            components: {
+                product,
             }
-        },
-        computed: {
-            products() {
-                return this.$store.state.activeProducts
-            },
-            notes() {
-
-                return this.$store.state.activeNotes[this.product._id]
-            }
-        },
-        components: {
         }
-
-    }
-</script>
-
-<style scoped>
-    .product-header {
-        display: flex;
-        justify-content: center;
-
-    }
-    /* .l{
-        background-color: rgb(253, 206, 147);
-        border-radius: 25px;
-        padding: .5rem .5rem .5rem .5rem;
-
-    } */
-    .title:hover{
-    color: rgb(131, 130, 130) 
-
-    }
-    .productt{
-        background-color: bisque;
-        border-radius: 25px;
-        margin: 1rem;
-        /* width: 30rem */
-    }
-
-    .open-notes {
-        cursor: pointer;
-
-    }
-
-    .fa-minus {
-        color: red;
-    }
-
-    .delProd {
-        /* float: right; */
-        color: rgb(235, 15, 15);
-    }
-
-    .delProd:hover {
-        color: rgb(255, 137, 137);
-    }
-
-    .fa-minus:hover {
-        color: rgb(255, 137, 137);
-    }
-
-    .product-container {
-        margin-left: 2rem;
-        margin-right: 2rem;
-        /* border: 1px solid black; */
-        /* border-radius: 25px; */
-        padding: 1rem;
-    }
-
-    .add-note {
-        /* padding-top: 5%; */
-        cursor: pointer;
-    }
-
-    .add-note:hover {
-        color: rgb(7, 7, 7);
-    }
-
-    .productTitle:hover {
-        color: rgb(2, 2, 2);
-    }
-
-    .productTitle {
-        display: inline;
-        /* padding-top: 1rem; */
-    }
-
-    .listSelect {
-        display: inline;
-    }
-
-    .notes {
-        margin: 2rem;
-        border: 1px solid black;
-        border-radius: 5px;
-        background: rgb(247, 250, 247);
-        word-wrap: normal;
-        padding-top: .5rem;
-    }
-
-    .inline {
-        display: inline;
-    }
-
-    .float {
-        float: right;
-    }
-</style>
+    </script>
+    <style>
+        .list {
+            border-radius: 5px;
+            height: 600px;
+            margin-top: 1%;
+            overflow: auto;
+            background: rgb(104, 142, 255);
+        }
+    
+        ::-webkit-scrollbar {
+            width: 5px;
+        }
+    
+        ::-webkit-scrollbar-track {
+            background: #f1f1f1;
+        }
+    
+        ::-webkit-scrollbar-thumb {
+            background: #888;
+        }
+    
+        .inline {
+            display: inline;
+        }
+    
+        .list-text-color {
+            color: rgb(3, 3, 3);
+        }
+    
+        .list-header {
+            padding: 1rem;
+        }
+        .list-footer{
+            display: ;
+            justify-content: space-around;
+        } 
+    
+        .margin {
+            margin: 1rem;
+        }
+    
+        .the-product {
+            margin-top: 0.5rem;
+            
+            /* width: 50rem; */
+        }
+    
+        /* .product {
+            padding-top: 1rem;
+            padding-bottom: 1rem;
+        } */
+    </style>
