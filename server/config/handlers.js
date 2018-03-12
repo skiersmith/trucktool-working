eb051ed664a287254bfa04d1022d2d6b4485f20f
Hunter Smith <skiersmith21@gmail.com>
2018-03-12 15:07:48 -0600

    build for deployment44

--- server/config/handlers.js
+++ server/config/handlers.js
@@ -22,7 +22,7 @@ let defaultErrorHandler = (err, req, res, next) => {
 }
 
 //Default Urls 
-var whitelist = ['http://localhost:8080', 'https://quiet-eyrie-56379.herokuapp.com/'];
+var whitelist = ['http://localhost:8080', 'https://serene-lowlands-35394.herokuapp.com/'];
 var corsOptions = {
 	origin: function (origin, callback) {
 		var originIsWhitelisted = whitelist.indexOf(origin) !== -1;
