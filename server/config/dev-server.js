9ea1472aa8fa473d4e67365d09519820b79b5c5a
Hunter Smith <skiersmith21@gmail.com>
2018-03-12 14:47:40 -0600

    build for deployment22

--- server/config/dev-server.js
+++ server/config/dev-server.js
@@ -29,10 +29,10 @@ function logger(req, res, next) {
 }
 
 // REGISTER MIDDLEWARE
-app.use(express.static('trucktool-server-vue' + '/../../www/dist'))
 app.use(session)
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.static(__dirname + '/../../www/dist'))
 app.use('*', logger)
 app.use('*', cors(corsOptions))
 app.use('/', Auth)
